628e1ef4677e10f2865ca047c3e7ef8bb14c758e
Asus <koncarik@mail.ru>
2022-11-29 22:44:50 +0500

    Задача решена, второе число выводится

---
+++ Zadacha_10/Program.cs
@@ -0,0 +1,8 @@
+﻿//Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа
+
+Console.Clear();
+int number =  int.Parse(Console.ReadLine());;
+Console.WriteLine("Введите трехзначное число: ");
+
+int second = number/10 % 10;
+Console.Write($"{number} -> {second}");
