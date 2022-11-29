d0bda8bad3ea6c01c428eab0970298609f479ded
Evgeniia-ne <118460169+Evgeniia-ne@users.noreply.github.com>
2022-11-29 22:47:45 +0500

    Update Program.cs

--- Zadacha_10/Program.cs
+++ Zadacha_10/Program.cs
@@ -1,7 +1,7 @@
 ﻿//Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа
 
 Console.Clear();
-int number =  int.Parse(Console.ReadLine());;
+int number =  int.Parse(Console.ReadLine());
 Console.WriteLine("Введите трехзначное число: ");
 
 int second = number/10 % 10;
